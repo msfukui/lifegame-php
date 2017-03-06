59b8ef322542883ad61dae6f03ab615b7b98c252
hikaruworld <hikaruworld@gmail.com>
2017-03-06 18:21:45 +0900

    ループでの一定回数の盤面の出力を実装

--- Main.php
+++ Main.php
@@ -15,3 +15,14 @@ for ($i = 0; $i < 5; $i++) {
     echo($line . PHP_EOL);
 }
 
+
+for ($times = 0; $times < 3; $times++) {
+    echo ($times . "==========" . PHP_EOL);
+    for ($i = 0; $i < 5; $i++) {
+        $line = "";
+        for ($j = 0; $j < 5; $j++) {
+            $line = $line . $b[$i][$j];
+        }
+        echo $line . PHP_EOL;
+    }
+}
